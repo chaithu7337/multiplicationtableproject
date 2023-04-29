7946e9d4be61a27b0c1d3e9c6f872fff8e931a4d
chaithu7337 <132151989+chaithu7337@users.noreply.github.com>
2023-04-29 20:50:30 +0530

    Add files via upload

---
+++ MultiplicationTableRunner.java
@@ -0,0 +1,12 @@
+package com.Firstjavaproject;
+
+public class MultiplicationTableRunner {
+
+	public static void main(String[] args) {
+		MultiplicationTable table = new MultiplicationTable();
+		//table.print();
+		table.print(70, 11, 20);
+
+	}
+
+}
